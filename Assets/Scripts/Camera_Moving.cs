e8e34376230cde5cf2efd9d0c60decf4556cfe60
SunSet <53521508+SunSet97@users.noreply.github.com>
2020-01-19 23:38:02 +0900

    *맵 상 카메라 이동제한 코드 완성, 정보 값 입력 오브젝트/스크립트 추가
    -기존 Test_Scene의 카메라 뷰 씬에서 카메라 뷰 관련 코드 수정 및 구현.
    -각 씬 맵마다 반복되는 변수에 일일이 값입력하는 노가다 없애기 위해 Information_Scene이라는 오브젝트와 스크립트 추가.
    -구현은 되어있지만 추후 더 수월한 작업을 위해서 추가작업 필요
    -추가작업 요구사항: Information_Scene 스크립트 인스턴스화, 이 오브젝트에 입력된 정보값을 수월하게 받을 수 있게 만드는 작업
    (아직 직접 Find().GetComponent<>().~~이런식으로 직접 불러와서 값을 사용하는데 구현은 되지만 번거로운 코드)

--- Assets/Scripts/Camera_Moving.cs
+++ Assets/Scripts/Camera_Moving.cs
@@ -4,148 +4,72 @@ using UnityEngine;
 
 public class Camera_Moving : MonoBehaviour
 {
-    public GameObject Player;
-    //카메라 Transform속성 저장할 변수와 Transform의 position값 저장할 변수
-    Transform Player_transform;
-    Vector3 Position_;
-    Vector3 Position_player;
-    public const float Z_Value=-10;
 
-    //카메라 경계값
-    [Header("카메라 경계값")]
-    public float min_x=-1f;
-    public float max_x=5f;
-    public float min_y=-2.0f;
-    public float max_y=1.3f;
+    //Player 오브젝트 받는 변수
+    public GameObject Player;
+
+    //Player의 Transform값 저장할 Transform변수
+    Transform Player_transform;
+
+    //각 카메라와 Player의 Position값 저장할 Vector3 변수
+    Vector3 Camera_Position;
+    Vector3 Player_Position;
+
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("작동");
-        Player=GameObject.Find("Player");
-        //플레이어와 카메라 오브젝트의 Transform 속성 객체화,각 position값 객체화
+        //씬정보, Player 오브젝트 찾기
+       
+        Player = GameObject.Find("Player");
+       
+        //Player의 Transform 속성 객체화
         Player_transform = Player.GetComponent<Transform>();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector3 Position;
-        Position_player = Player_transform.position;
+        //플레이어 위치 변수에 플레이어 위치 값 넣음.
+        Player_Position = Player_transform.position;
+        //메인 카메라의 위치를 플레이어 위치와 동일시함.
         Camera.main.transform.position = Player_transform.position;
-        Position_ = Camera.main.transform.position;
-        Follow_Player(Position_);
-        Player_transform.position=Position_player;
+        //지금 카메라의 위치를 카메라 위치변수에 넣음
+        Camera_Position = Camera.main.transform.position;
+        //카메라의 이동과 제한을 위한 함수에 매개변수로 카메라 위치변수 넣음.
+        Follow_Player(Camera_Position);
+        Player_transform.position = Player_Position;
 
     }
 
     void Follow_Player(Vector3 position)
     {
+        //값변경을 위한 지역변수
         float position_storage;
-        if (position.z != Z_Value)
+
+        //카메라 경계값정보 Information_Scene오브젝트에서 받아오기
+        float min_x = GameObject.Find("Information_Scene").GetComponent<Information_Scene>().min_x;
+        float max_x = GameObject.Find("Information_Scene").GetComponent<Information_Scene>().max_x;
+        float min_y = GameObject.Find("Information_Scene").GetComponent<Information_Scene>().min_y;
+        float max_y = GameObject.Find("Information_Scene").GetComponent<Information_Scene>().max_y;
+        float Z = GameObject.Find("Information_Scene").GetComponent<Information_Scene>().Z_Value;
+
+        //카메라 위치 제한 설정
+        position.x = Mathf.Clamp(position.x,min_x,max_x);
+        position.y= Mathf.Clamp(position.y, min_y, max_y);
+        Camera.main.transform.position = position;
+
+        //카메라의 Z값 고정을 위한 If문
+        if (position.z != Z)
         {
-            position_storage = Z_Value;
+            position_storage = Z;
             position.z = position_storage;
             Camera.main.transform.position = position;
         }
-
-        //카메라 위치가 일정 위치에서 벗어날 때 멈추기
-        //X값
-        if (position.x > max_x)
-        {
-            Number_x(position, max_x);
-            if (position.y > max_y)
-            {
-                Number_y(position, max_y);
-            }
-            else if (Position_.y < min_y)
-            {
-                Number_y(position, min_y);
-            }
-            Number_y(position, position.y);
-        }
-        else if (position.x < min_x)
-        {
-            Number_x(position, min_x);
-            if (position.y > max_y)
-            {
-                Number_y(position, max_y);
-            }
-            else if (Position_.y < min_y)
-            {
-                Number_y(position, min_y);
-            }
-            Number_y(position, position.y);
-        }
-        else if(position.x <= max_x&& position.x >= min_x)
-        {
-            if (position.y > max_y)
-            {
-                Number_y(position, max_y);
-            }
-            else if (Position_.y < min_y)
-            {
-                Number_y(position, min_y);
-            }
-
-        }
-
-
-        //Y값
-        if (position.y > max_y)
-        {
-            Number_y(position, max_y);
-            if (position.x > max_x)
-            {
-                Number_x(position, max_x);
-            }
-            else if (position.x < min_x)
-            {
-                Number_x(position, min_x);
-            }
-            Number_x(position, position.x);
-        }
-        else if (Position_.y < min_y)
-        {
-            Number_y(position, min_y);
-            if (position.x > max_x)
-            {
-                Number_x(position, max_x);
-            }
-            else if (position.x < min_x)
-            {
-                Number_x(position, min_x);
-            }
-            Number_x(position, position.x);
-        }
-        else if (position.y <= max_y && position.y >= min_y)
-        {
-            if (position.x > max_x)
-            {
-                Number_x(position, max_x);
-            }
-            else if (Position_.x < min_x)
-            {
-                Number_x(position, min_x);
-            }
-        }
-            
-        Debug.Log("작동");
-    }
-
-    void Number_x(Vector3 position,float number)
-    {
-        float position__;
-        position__ = number;
-        position.x = position__;
-        Camera.main.transform.position = position;
-    }
-    void Number_y(Vector3 position, float number)
-    {
-        float position__;
-        position__=number;
-        position.y = position__;
-        Camera.main.transform.position = position;
     }
 
 }
+
+ 
---
+++ Assets/Scripts/Information_Scene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Information_Scene : MonoBehaviour
+{
+    /*
+     이 스크립트는 각 씬마다 '중복'되는 정보값을 일일이 스크립팅하는 짓을
+     막기 위해 각 씬마다 <Scene_Information>오브젝트에 값입력하기만 하면 되는
+     (타 스크립트에서 이 스크립트에서 정보값을 받도록 스크립트 짰다는 한에서)
+     스크립팅 노가다 방지용 스크립트/오브젝트입니다.
+
+    중복되는 값: 카메라 경계값+
+    ※추후 추가되는 변수 값들은 밑의 양식으로 적어주십쇼※
+    
+    */
+
+    [Header("카메라 경계값")]
+    public float min_x = -1f;
+    public float max_x = 5f;
+    public float min_y = -2.0f;
+    public float max_y = 1.3f;
+    public float Z_Value = -10; //카메라 Z값 고정하기 위한 상수값
+
+    /*나중에 인스턴스화 해야 될 듯. 각 씬마다 이 오브젝트 유무 판단하는 스크립트 필수*/
+}
