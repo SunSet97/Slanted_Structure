0b230ca4bf64dd9a207e131dd416d1dd40f6e5b1
gongwonSA <gongwon@ajou.ac.kr>
2020-06-26 00:40:04 +0900

    어셋추가

---
+++ Assets/Prefabs/Minigame/MinigameUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameUI : MonoBehaviour
+{
+    public GameObject map;
+    public GameObject ui;
+    
+    void Update()
+    {
+        ui.SetActive(map.activeSelf);
+    }
+}
---
+++ Assets/Speat_test/Move_Speat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Speat : MonoBehaviour
+{
+    public int speed = 6; //이동 속도
+    public Joystick joy; //조이스틱
+    Vector3 joy_v; //조이스틱 벡터
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Moving();
+    }
+    void Moving()
+    {
+        joy_v.Set(joy.Horizontal * speed * Time.fixedDeltaTime, 0, 0); //조이스틱의 horizontal값으로 벡터 set
+        gameObject.transform.position += joy_v;
+
+       // Debug.Log(joy_v.x);
+        if (joy_v.x < 0 && transform.eulerAngles.y != 90) //방향 전환(왼쪽)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+        }
+        else if (joy_v.x > 0 && transform.eulerAngles.y != -90) //방향 전환(오른쪽)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
+        }
+    }
+}
