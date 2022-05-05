3d44943497bfe858ccc245e8d862c34675471323
SunSet <53521508+SunSet97@users.noreply.github.com>
2022-05-05 14:09:28 +0900

    패키지 삭제, 206140 맵 시네마틱 임의
    206140프리팹은 Timeline>Test 폴더에 있음

--- Excel2Json/bin/example.bat
+++
@@ -1,5 +0,0 @@
-set CURPATH=%cd%
-
-..\Excel2Json.exe 
-..\Excel2Json -f %CURPATH%\example.xlsx -o .\
-pause
--- Excel2Json/bin/task.bat
+++
@@ -1,6 +0,0 @@
-set CURPATH=%cd%
-
-..\Excel2Json.exe 
-..\Excel2Json -f %CURPATH%\task.xlsx -o .\
-
-pause
