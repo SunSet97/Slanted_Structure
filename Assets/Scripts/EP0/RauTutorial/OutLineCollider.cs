b1c50181a5a5a1fa4635544c332dfe46f3fb3ae4
UISU <hanaaple@ajou.ac.kr>
2022-06-05 17:29:28 +0900

    001010, 001011 코드 최적화 - Grass Outline
    Outline 키고 끄는 전용 스크립트 추가

---
+++ Assets/Scripts/EP0/RauTutorial/OutLineCollider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutLineCollider : MonoBehaviour
+{
+    [Header("#Outline setting")]
+    public Color outlineColor = Color.red;
+
+    public Vector3 offset;
+    public float radius;
+    public Outline.Mode mode = Outline.Mode.OutlineAll;
+    public Outline outline;
+
+    void Start()
+    {
+        if(outline == null)
+            outline = gameObject.AddComponent<Outline>();
+        outline.OutlineMode = mode;
+        outline.OutlineColor = outlineColor;
+        outline.OutlineWidth = 8f;
+        outline.enabled = false;
+        SphereCollider sphereCol;
+        if (!gameObject.TryGetComponent(out sphereCol)) 
+            sphereCol = gameObject.AddComponent<SphereCollider>();  // 콜라이더 없으면 자동 추가
+        sphereCol.isTrigger = true;
+        sphereCol.center = offset / transform.lossyScale.y;
+        sphereCol.radius = radius / transform.lossyScale.y;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        outline.enabled = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        outline.enabled = false;
+    }
+}
