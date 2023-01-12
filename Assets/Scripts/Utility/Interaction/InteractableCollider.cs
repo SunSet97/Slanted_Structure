dfd1e3a4759768f76ff703a81eaea64f5c0e8374
Uisu Seo <hanaaple@ajou.ac.kr>
2023-01-12 15:11:32 +0900

    Add Interactable Collider (Temp)

---
+++ Assets/Scripts/Utility/Interaction/InteractableCollider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Utility.Interaction
+{
+    public class InteractableCollider : MonoBehaviour
+    {
+        [SerializeField] private InteractionObj_stroke interactionObjStroke;
+    
+        private void OnTriggerEnter(Collider other)
+        {
+        
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+        
+        }
+    }
+}
